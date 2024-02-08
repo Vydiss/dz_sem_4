876d0a78b4ba19bda0b5c32e46e2d63d7d82b78f
Endruman <hayllihuk@gmail.com>
2024-02-08 13:36:26 +0300

    Задача: 3, Решена

--- Program.cs
+++ Program.cs
@@ -29,33 +29,66 @@
 
 // Задача 2: Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
-Console.Clear();
+// Console.Clear();
 
-int[] CreateArray(int size)
-{
-    int[] array = new int[size]; // новый массив на size элементов
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(100, 1000);
-    }
-    return array;
-}
+// int[] CreateArray(int size)
+// {
+//     int[] array = new int[size]; // новый массив на size элементов
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(100, 1000);
+//     }
+//     return array;
+// }
 
-int CountEvenNumbers(int[] numbers)
-{
-    int count = 0;
-    foreach (int number in numbers)
-    {
-        if (number % 2 == 0)
-        {
-            count++;
-        }
-    }
-    return count;
-}
+// int CountEvenNumbers(int[] numbers)
+// {
+//     int count = 0;
+//     foreach (int number in numbers)
+//     {
+//         if (number % 2 == 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
 
-Console.Write("Введите размер массива: ");
-int sizeArray = Convert.ToInt32(Console.ReadLine());
-int[] arr = CreateArray(sizeArray);
-Console.WriteLine($"Массив: [ {string.Join("; ", arr)} ]");
-Console.WriteLine($"Количество четных чисел: {CountEvenNumbers(arr)}");
+// Console.Write("Введите размер массива: ");
+// int sizeArray = Convert.ToInt32(Console.ReadLine());
+// int[] arr = CreateArray(sizeArray);
+// Console.WriteLine($"Массив: [ {string.Join("; ", arr)} ]");
+// Console.WriteLine($"Количество четных чисел: {CountEvenNumbers(arr)}");
+
+
+// Задача 3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+// Console.Clear();
+
+// int[] CreateArray(int size, int min, int max)
+// {
+//     int[] array = new int[size]; // новый массив на size элементов
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(min, max + 1);
+//     }
+//     return array;
+// }
+
+// int[] ReverseArray(int[] array)
+// {
+//     int[] revArray = new int[array.Length];
+//     for (int i = 0, j = array.Length - 1; i < array.Length; i++, j--)
+//     {
+//         revArray[j] = array[i];
+//     }
+//     return revArray;
+// }
+
+// Console.WriteLine("Введите длинну массива: ");
+// int sizeArr = Convert.ToInt32(Console.ReadLine());
+// int[] array = CreateArray(sizeArr, 1, 150);
+// int[] revArray = ReverseArray(array);
+
+// Console.WriteLine($"Заданный массив: [ {string.Join("; ", array)} ]");
+// Console.WriteLine($"Перевернутый массив: [ {string.Join("; ", revArray)} ]");
